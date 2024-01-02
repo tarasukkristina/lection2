91148b892afe1d83fccd09a7cdcef4cf6e1e42dd
Kristina Tarasuk <tarasukkristina92@gmail.com>
2024-01-02 14:27:22 +0300

    added the solution of task-1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
+        //int number = 123123123;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int number = scanner.nextInt();
+
+        String numberStr = String.valueOf(number);
+        int numberStrLength = numberStr.length();
+
+        String lastdigit = numberStr.substring(numberStrLength -1);
+        System.out.println(lastdigit);
+
+
+
 
     }
 }
